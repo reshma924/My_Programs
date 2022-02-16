09f0df328d2589ca99af0d1de97fd62762c37b8d
reshma924 <98262879+reshma924@users.noreply.github.com>
2022-02-16 09:40:06 +0530

    Add files via upload

---
+++ JavaSamplePrograms/decimal_binary.java
@@ -0,0 +1,13 @@
+package Java_Programs;
+
+public class decimal_binary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(Integer.toBinaryString(10));
+System.out.println(Integer.toBinaryString(15));
+System.out.println(Integer.toBinaryString(12));
+
+	}
+
+}
---
+++ JavaSamplePrograms/decimal_hex.java
@@ -0,0 +1,12 @@
+package Java_Programs;
+
+public class decimal_hex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String hex="a";
+int decimal =Integer.parseInt(hex, 16);
+System.out.println(decimal);
+	}
+
+}
